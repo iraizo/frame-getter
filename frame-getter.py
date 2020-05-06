67ac6d8e6b0ada25ee46c378ccf05004a048d18c
raizo <35413558+iraizo@users.noreply.github.com>
2020-05-06 15:05:36 +0200

    update

--- frame-getter.py
+++ frame-getter.py
@@ -27,5 +27,5 @@ files = os.listdir(directory + "\\input") # update files if needed, probaly not
 # actually run ffmpeg
 for video in files:
   name = uuid.uuid1().int
-  print(system(f'ffmpeg -i "C:\\Users\\raizo\\Desktop\\frames\\input\\{video}" -vf "select=\'if(not(floor(mod(t,5)))*lt(ld(1),1),st(1,1)+st(2,n)+st(3,t));if(eq(ld(1),1)*lt(n,ld(2)+8),1,if(trunc(t-ld(3)),st(1,0)))\'" -vsync 0 {dir_output}{name}_out%d.png'))
+  print(system(f'ffmpeg -i "{directory}\\{video}" -vf "select=\'if(not(floor(mod(t,5)))*lt(ld(1),1),st(1,1)+st(2,n)+st(3,t));if(eq(ld(1),1)*lt(n,ld(2)+8),1,if(trunc(t-ld(3)),st(1,0)))\'" -vsync 0 {dir_output}{name}_out%d.png'))
 
